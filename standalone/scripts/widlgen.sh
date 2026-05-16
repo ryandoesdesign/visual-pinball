ed6d51e10395ac39dbcd7a9d20b3700920a84878
Ryan Tessier <tessier.ryan.m@gmail.com>
2026-05-16 19:17:19 +1000

    chore(platforms): drop non-macos-arm64 platforms and build scaffolding
    Remove every platform target except macOS arm64:
    
    - platforms/: drop macos-x64, ios-arm64, ios-simulator-arm64,
      android-arm64-v8a, linux-{aarch64,x64}, windows-{x64,x86,mingw-x64}.
    - make/: drop CMakeLists for those targets, plus DX9 CMakeLists,
      MSVC vcxproj/sln/natvis/bat/wsf artifacts, and CMakeLists_bgfx_lib.txt
      (iOS/Android shared-library build entry).
    - standalone/: drop android, ios, docs/ios, idl, inc/win32xx, scripts/.
      Drop Windows COM scaffolding (vpinball_standalone_i*, PoleStorage*,
      PoleStream*, FreeImage.h).
    - lib/: drop entirely. The whole tree (VPinballLib*, WebServer, ZipUtils,
      VPXProgress, bindings/java) was only consumed under __LIBVPINBALL__
      by the iOS/Android lib build; standalone macOS does not use it.
    
    Dead __LIBVPINBALL__ ifdefs in src/ will be cleaned in a later phase.

--- standalone/scripts/widlgen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-WINE_PATH=/Users/jmillard/vpx/wine
-
-${WINE_PATH}/tools/widl/widl -m64 -o ../vpinball_standalone_i.h --nostdinc -Ldlls/\* -I${WINE_PATH}/include -D__WINESRC__ -D_UCRT ../../src/core/vpinball.idl
-${WINE_PATH}/tools/widl/widl -m64 -u -o ../vpinball_standalone_i.c --nostdinc -Ldlls/\* -I${WINE_PATH}/include -D__WINESRC__ -D_UCRT ../../src/core/vpinball.idl
